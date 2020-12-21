d9efb569d0a045918d776a0077cd7a2cf098a441
HeenaR17 <70769973+HeenaR17@users.noreply.github.com>
2020-12-21 13:40:25 +0530

    Add files via upload

---
+++ Snakegame - Copy/app/src/main/java/com/pixelsface/snake/AppConstants.java
@@ -0,0 +1,12 @@
+package com.pixelsface.snake;
+
+
+public final class AppConstants {
+
+    public static final String CONTROL_KEY = "CONTROL_KEY";
+    public static final String SOUND_KEY = "SOUND_KEY ";
+
+    public enum Control {POV, DUAL, SPLIT}
+    // For tracking movement m_Direction
+    public enum Direction {UP, RIGHT, DOWN, LEFT}
+}
